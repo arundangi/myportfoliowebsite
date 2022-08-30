cb807ab8a5682d77c7298b82caa822fb6464cec2
arundangi <arund1127@gmail.com>
2022-08-30 12:46:05 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,189 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "C",
+      img:
+        "https://1.bp.blogspot.com/-nxid6Xlc4tk/WZdTjNSFYeI/AAAAAAAAFnk/DAH_PDBuU2AnHjF7eEf6JzoKO9yXguUhACLcBGAs/s1600/Top%2B10%2Bvideo%2Btutorial%2Bto%2Blearn%2BC%2BProgramming.jpg",
+    },
+    {
+      id: 2,
+      title: "C++",
+      img:
+        "https://image.shutterstock.com/image-photo/c-programming-language-large-lettering-260nw-1770351188.jpg",
+    },
+    {
+      id: 3,
+      title: "Python(Basic)",
+      img:
+        "http://d1jnx9ba8s6j9r.cloudfront.net/blog/wp-content/uploads/2017/06/Python-Programming-Edureka.png",
+    },
+    // {
+    //   id: 4,
+    //   title: "Relax Mobile App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Hero Web Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "HTML",
+      img: "https://www.udacity.com/blog/wp-content/uploads/2020/06/HTML_Blog-scaled.jpeg",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      img: "https://www.elegantthemes.com/blog/wp-content/uploads/2021/01/000-Basic-CSS.png",
+    },
+    {
+      id: 3,
+      title: "Js",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcQbnpMKOhgOIMOB5T68UZaHIquEDvnkqSrw&usqp=CAU",
+    },
+    {
+      id: 4,
+      title: "ReactJs",
+      img: "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
+    },
+    // {
+    //   id: 5,
+    //   title: "Web Design",
+    //   img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Web Banking App",
+    //   img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: "Data Structure",
+      img:
+        "https://i.ytimg.com/vi/Qmt0QwzEmh0/maxresdefault.jpg",
+    },
+    {
+      id: 2,
+      title: "Operating System",
+      img:
+        "https://image.shutterstock.com/image-photo/business-technology-internet-network-concept-260nw-559459693.jpg",
+    },
+    {
+      id: 3,
+      title: "Computer Networks",
+      img:
+        "https://www.static-contents.youth4work.com/university/Documents/Colleges/studymaterials/1e8cbf66-5dff-4541-a9ab-cdac7147f816.jpg",
+    },
+    // {
+    //   id: 4,
+    //   title: "Mobile Relax App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Mobile Hero Design",
+    //   img:
+    //     "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Mobile Banking App",
+    //   img:
+    //     "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    // },
+  ];
+  
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: "AWS",
+      img:
+        "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_aws.jpg",
+    },
+    {
+      id: 2,
+      title: "Cloud",
+      img:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyPmoxUVvFqhzCHhCjK_egOw_Js9uSF510EA&usqp=CAU",
+    },
+    {
+      id: 3,
+      title: "DSA",
+      img:
+        "https://image.shutterstock.com/image-vector/dsa-creative-logo-monogram-white-260nw-1835276254.jpg",
+    },
+    {
+      id: 4,
+      title: "Nmap",
+      img:
+        "https://www.tecmint.com/wp-content/uploads/2016/11/Nmap-Kali-Linux-Guide.png",
+    },
+    {
+      id: 5,
+      title: "Metasploit",
+      img:
+        "https://miro.medium.com/max/780/1*-mVgJOnMCM9Xu_1CIhVMqQ.jpeg",
+    },
+    {
+      id: 6,
+      title: "Wireshark",
+      img:
+        "https://static.wixstatic.com/media/0f2726_d671f45b714240bcb133339406c47672~mv2.png/v1/fill/w_498,h_496,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/0f2726_d671f45b714240bcb133339406c47672~mv2.png",
+    },
+  ];
+  
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: "CCNA",
+      img:
+        "https://wagtail-prod-storage.s3.amazonaws.com/images/ccna.2e16d0ba.fill-40000x30000.jpg",
+    },
+    {
+      id: 2,
+      title: "AWS Cloud",
+      img:
+        "https://asapkerala.gov.in/wp-content/uploads/2021/10/AWS-Cloud-Foundation.jpg",
+    },
+    // {
+    //   id: 3,
+    //   title: "Content E-commerce Web Design",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Content Relax Mobile App",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Content Keser Web Design",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Content Banking App",
+    //   img:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    // },
+  ];
+  
